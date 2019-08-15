29f754ad4e8011fa2f5e955f7dcbda80d9e9624b
GucciLee <guccilee@163.com>
2019-08-15 18:15:08 +0800

    'vee-validate'

--- src/conf/vee-validate/validator.js
+++ src/conf/vee-validate/validator.js
@@ -9,14 +9,33 @@ Vue.use(VeeValidate, Config);
 // 自定义验证规则
 const customRules = {
     //手机号验证
-    isMobile: {
+    phone: {
         getMessage: (field, params, data) => '格式不正确',
         validate: (value, args) => {
             return false
         }
+    },
+    unique: {
+        getMessage: (field, params, data) => data.message,
+        validate: value =>
+        new Promise(resolve => {
+            if (0) {
+                return resolve({
+                    valid: true
+                });
+            }
+
+            return resolve({
+                valid: false,
+                data: {
+                    message: `${value} 已经存在.`
+                }
+            });
+        }),
     }
 };
-Validator.extend('liguanjie', customRules.isMobile);
+Validator.extend('phone', customRules.phone);
+Validator.extend('unique', customRules.unique);
 
 // 自定义验证消息
 Validator.localize({zh_CN: Langule.zh_CN});
